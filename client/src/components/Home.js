8d76aec712d59013284d42516e941b3fb1b0302f
Shrishesh Pratik <shrisheshpratik@gmail.com>
2021-07-07 12:37:28 -0400

    React Setup + Responsive Navbar

---
+++ client/src/components/Home.js
@@ -0,0 +1,13 @@
+// @flow
+import React from 'react';
+
+export const Home = () => {
+    return (
+        <div>
+            <p>Welcome</p>
+            <h1>We are MERN Developer</h1>
+        </div>
+    );
+};
+
+export default Home;
